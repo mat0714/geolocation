558227947b26a86cc0d0060f7b46bc8529db51ec
Mateusz <mateusz0714@gmail.com>
2022-12-27 12:19:25 +0100

    Fix problem with authorization

--- src/main/java/pl/mateuszkolodziejczyk/geolocation/security/SecurityConfig.java
+++ src/main/java/pl/mateuszkolodziejczyk/geolocation/security/SecurityConfig.java
@@ -1,18 +1,23 @@
 package pl.mateuszkolodziejczyk.geolocation.security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests().anyRequest().authenticated();
-        http.httpBasic();
+        http
+                .authorizeHttpRequests().anyRequest().authenticated()
+                .and()
+                .httpBasic()
+                .and()
+                .csrf().disable();
         return http.build();
     }
 }
